b1988295388e42c5797e82fe75ff2a37e15bb895
sarayut27 <68211053+sarayut27@users.noreply.github.com>
2020-08-25 21:42:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace oop_6115261027_w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room r434 = new Room("434", "434", 4, 4, "Computer");
+            Console.WriteLine(r434);
+            Building b4 = new Building("4 ", " Computer ", " 5.456 ", " 562.654 ");
+            Console.WriteLine(b4);
+            Subject th = new Subject("33235 ", " thai ", " 2 ", " 2 ", " 2 ");
+            Console.WriteLine(th);
+            Lecturer tc = new Lecturer("Sarayut ", " Tabteesud ", " Teacher ");
+            Console.WriteLine(tc);
+            Section aa = new Section("341 ", " 01 ", " Computer ", " T.arm ", " 01 Aug ", " 13.00 ", " 15.30 ");
+            Console.WriteLine(aa);
+            Programm cc = new Programm("Com ", " Bachelor degree ");
+            Console.WriteLine(cc);
+        }
+    }
+}
